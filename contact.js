5f9718901e176ceb3b5eba30030bc8a13d08947c
Nazav7 <nazarenavazquez07@gmail.com>
2024-07-31 17:03:41 -0300

    Updated contact page(html,css,js)

--- contact.js
+++ contact.js
@@ -1,23 +1,23 @@
+"use Strict";
+
+const form = document.querySelector("form");
 const firstName = document.getElementById("name");
 const lastName = document.getElementById("last-name");
-const email = document.getElementById("email")
-const phone = document.getElementById("phone")
-const message = document.getElementById("message")
-const sendMessageButton = document.getElementById("send");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const message = document.getElementById("message");
 let information = [];
 
-sendMessageButton.addEventListener("submit", (e) => {
-    e.preventDefault(); // previene la acción del form de actualizar la página
-    information[0] = firstName.value;
-    information[1] = lastName.value;
-    information[2] = email.value;
-    information[3] = phone.value;
-    information[4] = message.value;
+form.addEventListener("submit", (e) => {
+        e.preventDefault(); // Previene la acción predeterminada si el formulario es válido
+        information[0] = firstName.value;
+        information[1] = lastName.value;
+        information[2] = email.value;
+        information[3] = phone.value;
+        information[4] = message.value;
 
-    //console.log(`Su nombre es ${informacion[0]} y su apellido es ${informacion[1]}`);
+        let blob = new Blob([information], { type: "text/plain;charset=utf-8" }); // navegador
 
-    let blob = new Blob([information], { type: "text/plain;charset=utf-8" }); // navegador
-
-    // Librería FileSaver.js
-    saveAs(blob, "contact.txt");
+        // Librería FileSaver.js
+        saveAs(blob, "contact.txt");
 });
